5b6267743d73cbec76d96a5f710b8cab97a4ce19
Max Bazarov <bazaroffm@gmail.com>
2022-11-28 21:39:22 +0300

    add payments service

--- payments/services.py
+++ payments/services.py
@@ -1,5 +1,36 @@
+import os
+
+import dotenv
 import stripe
+
+from .models import Item
+
+dotenv.load_dotenv()
+
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
 
 class PaymentSessionService:
-    pass
+
+    def create_session(self, item_id: int) -> int:
+        item = Item.objects.get(id=item_id)
+        session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': item.name,
+                        },
+                        'unit_amount': int(item.price * 100),
+
+                    },
+                    'quantity': 1,
+                }
+            ],
+            mode='payment',
+            success_url='http://localhost:8000/api/success',
+            cancel_url='http://localhost:8000/api/cancel',
+        )
+
+        return session.id
