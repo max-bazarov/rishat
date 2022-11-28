21677173c83db0a0aa75030892ace95b88a70e1c
Max Bazarov <bazaroffm@gmail.com>
2022-11-28 21:41:20 +0300

    configure urls

--- rishat_payments/urls.py
+++ rishat_payments/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic import RedirectView
 
 from .yasg import urlpatterns as yasg_urls
